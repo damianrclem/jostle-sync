36b6890c331324cccd9665e2d58d7eb7667ccb4c
Jared Brown <brown.jared25@gmail.com>
2022-04-14 14:35:41 -0400

    Placeholder endpoint

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,15 @@
 import 'express-async-errors'
-import express from 'express'
+import express, { Request, Response } from 'express'
 import { expressErrorHandler } from '../helpers/expressErrorHandler'
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json());
+
+app.post(('/sync'), async (req: Request, res: Response) => {
+  res.status(200).send()
+})
 
 app.use(expressErrorHandler);
 
