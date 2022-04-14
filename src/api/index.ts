495a54b5801cfe33b564827a2a45363018ea575a
Jared Brown <brown.jared25@gmail.com>
2022-04-14 14:41:27 -0400

    /sync endpoint triggers sync workflow

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,24 @@
 import 'express-async-errors'
-import express, { Request, Response } from 'express'
+import express, { Response } from 'express'
 import { expressErrorHandler } from '../helpers/expressErrorHandler'
+import { getWorkflowClient } from '../temporal/client'
+import { syncJostleUsersWorkflow } from '../workflows'
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json());
 
-app.post(('/sync'), async (req: Request, res: Response) => {
-  res.status(200).send()
+app.post(('/sync'), async (_, res: Response) => {
+  const client = getWorkflowClient();
+  const workflowResponse = await client.start(syncJostleUsersWorkflow, {
+    taskQueue: 'jostle-ad-sync',
+    workflowId: `jostle-ad-sync-${Date.now()}`
+  });
+  
+  res.status(200).send({
+    workflowId: workflowResponse.workflowId,
+  })
 })
 
 app.use(expressErrorHandler);
