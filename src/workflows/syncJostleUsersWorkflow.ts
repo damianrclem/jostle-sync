67c6712c127cc0f5db69d3faa3c68e026397037e
Damian Clem <damianrclem@gmail.com>
2022-04-21 12:37:25 -0700

    fixed typo

--- src/workflows/syncJostleUsersWorkflow.ts
+++ src/workflows/syncJostleUsersWorkflow.ts
@@ -59,7 +59,7 @@ interface SyncJostleUsersResult {
   };
 }
 
-interface UpdateUserRestuls {
+interface UpdateUserResults {
   updateUsersManager: string;
   UserID: string;
   UsersManager: string;
@@ -92,7 +92,7 @@ export async function syncJostleUsersWorkflow(): Promise<void> {
     // Finally, update users manager in AD
     await updateUsersManager(managerUserList[i].userId, manager.id);
 
-    const updateUserResults: UpdateUserRestuls = {
+    const updateUserResults: UpdateUserResults = {
       updateUsersManager: managerUserList[i].displayName,
       UserID: managerUserList[i].userId,
       UsersManager: manager.displayName,
