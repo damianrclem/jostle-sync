58ccdf14d12dfbd6049adfc6dd8c51e12edb874b
Damian Clem <damianrclem@gmail.com>
2022-04-22 19:06:45 -0700

    used map instead of foreach approach.

--- src/activities/getSharepointManagersList.ts
+++ src/activities/getSharepointManagersList.ts
@@ -4,7 +4,7 @@ import { UsersManagerListResponse } from '../types';
 export const getSharepointManagersListFactory = (microsoftGraphApiClient: MicrosoftGraphClient) => ({
   getSharepointManagersList: async (): Promise<Array<UsersManagerListResponse> | undefined> => {
     const response = await microsoftGraphApiClient.getSharepointManagerList();
-    const userManagerList = [];
+    // const userManagerList = [];
 
     if (!response?.value) {
       return undefined;
@@ -12,14 +12,11 @@ export const getSharepointManagersListFactory = (microsoftGraphApiClient: Micros
 
     // Build readable object to work with
     // TODO: the field properties for the returned list will need updated
-    for (let i = 0; i < response?.value.length; i += 1) {
-      userManagerList.push({
-        displayName: response.value[i].fields.field_6,
-        userId: response.value[i].fields.field_16,
-        managerLookupId: response.value[i].fields.Assigned_x0020_ManagerLookupId!,
-      });
-    }
 
-    return userManagerList as Array<UsersManagerListResponse>;
+    return response?.value.map((user) => ({
+      displayName: user.fields.field_6,
+      userId: user.fields.field_16,
+      managerLookupId: user.fields.Assigned_x0020_ManagerLookupId!,
+    }));
   },
 });
